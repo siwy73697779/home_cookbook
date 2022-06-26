ed30fd78fdf3ed24b90faaaafccdb33244e5c046
siwy73697779 <104018985+siwy73697779@users.noreply.github.com>
2022-06-26 15:13:06 +0200

    Add files via upload

--- home_cookbook/base/database.php
+++ home_cookbook/base/database.php
@@ -1,10 +1,11 @@
 <?php
-$hostName = "localhost";
-$userName = "root";
-$password = "";
-$databaseName = "home_cookbook";
- $conn = new mysqli($hostName, $userName, $password, $databaseName);
-// Check connection
+$hostName = "localhost"; // adres IP bazy danych MySQL
+$userName = "root"; // użytkownik bazy danych MySQL
+$password = ""; // hasło do użytkownika bazy danych MySQL
+$databaseName = "home_cookbook"; // nazwa bazy danych MySQL
+  $conn = new mysqli($hostName, $userName, $password, $databaseName);
+
+  // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
