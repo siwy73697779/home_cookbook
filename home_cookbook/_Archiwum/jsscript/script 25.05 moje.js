978ed1a3b1a3c67dc3b1c12092c5372e1b2adf63
siwy73697779 <104018985+siwy73697779@users.noreply.github.com>
2022-06-26 15:27:56 +0200

    Add files via upload

---
+++ home_cookbook/_Archiwum/jsscript/script 25.05 moje.js
@@ -0,0 +1,41 @@
+document.querySelectorAll(".btn_nav").forEach(v => {v.onclick=
+	function (wybor_menu) {
+		const http = new XMLHttpRequest()
+		
+		http.onreadystatechange = function (check) {
+			if (http.readyState===4) {
+				if (http.status===200) {
+					http.addEventListener("click", Nawigacja());
+					http.addEventListener("click", Wyroznienie());					
+				}
+			}
+		}
+		
+		function Nawigacja() {
+			document.querySelector("main").innerHTML = http.responseText
+		}
+		
+		function Wyroznienie(){
+			wybor_menu.target.classList.add("btn_nav_click")
+			//Powyższe powoduje natychmiastową zmianę stylu po kliknięciu w nawigacji
+
+			$(document).ready(function () {
+				$(".btn_nav").click(function () {
+				$(".btn_nav").removeClass("btn_nav_click");
+				//$(this).addClass("btn_nav_click");
+				//Piertwsze naciśnięcie nie powoduje zmiany stylu. 
+				});
+			  });			
+		}
+
+		
+		http.open("get",`./sites/${v.dataset.link}.html`)
+		http.send();
+	}
+})
+
+
+
+
+
+
